c9c3328cd659677e557c5aa7ff5b2c1626f1e951
John <john.h95@gmail.com>
2022-01-08 09:38:10 -0500

    Added starter code

---
+++ src/main/java/com/sealsugar/gymapp/GymAppApplication.java
@@ -0,0 +1,13 @@
+package com.sealsugar.gymapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sealsugar/gymapp/GymAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sealsugar.gymapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
