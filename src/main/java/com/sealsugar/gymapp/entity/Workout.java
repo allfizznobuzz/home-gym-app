75a2ceeaaf80346d8be7f9b402f73eeb7b4dfd2b
john.h95@gmail.com <Hur2dle1!>
2022-01-14 19:22:24 -0500

    Worked on workout classes

--- src/main/java/com/sealsugar/gymapp/entity/Workout.java
+++ src/main/java/com/sealsugar/gymapp/entity/Workout.java
@@ -2,18 +2,33 @@ package com.sealsugar.gymapp.entity;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.List;
+
+import static javax.persistence.GenerationType.SEQUENCE;
 
 @NoArgsConstructor
 @Data
 @Entity(name = "Workout")
 public class Workout {
 
-    @Id()
-    private int workoutId;
+    @Id
+    @SequenceGenerator(
+            name = "workout_sequence",
+            sequenceName = "workout_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "workout_sequence"
+    )
+    @Column(
+            name = "workout_id",
+            updatable = false
+    )
+    private Long workoutId;
     private String workoutName;
     private WorkoutDetails workoutDetails;
     private String workoutLevel;
--- src/main/java/com/sealsugar/gymapp/entity/WorkoutDetails.java
+++ src/main/java/com/sealsugar/gymapp/entity/WorkoutDetails.java
@@ -1,17 +1,21 @@
 package com.sealsugar.gymapp.entity;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @NoArgsConstructor
 @Data
 @Entity (name = "workout_details")
 public class WorkoutDetails {
 
-    @Id()
+    @Column(name = "description")
     private String description;
     private int sets;
     private int reps;
