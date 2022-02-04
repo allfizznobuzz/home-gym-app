154709843bb25fd360cd6ca2edcc58e7d6461179
John <john.h95@gmail.com>
2022-02-03 20:40:21 -0500

    Removed toString annotation.

--- src/main/java/com/sealsugar/gymapp/controller/WorkoutController.java
+++ src/main/java/com/sealsugar/gymapp/controller/WorkoutController.java
@@ -1,8 +1,6 @@
 package com.sealsugar.gymapp.controller;
 
 import com.sealsugar.gymapp.entity.Workout;
-import com.sealsugar.gymapp.entity.WorkoutDetails;
-import com.sealsugar.gymapp.repository.WorkoutRepository;
 import com.sealsugar.gymapp.service.WorkoutService;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,11 +10,9 @@ import java.util.List;
 public class WorkoutController {
 
     WorkoutService workoutService;
-    WorkoutRepository workoutRepository;
 
-    public WorkoutController(WorkoutService workoutService, WorkoutRepository workoutRepository) {
+    public WorkoutController(WorkoutService workoutService) {
         this.workoutService = workoutService;
-        this.workoutRepository = workoutRepository;
     }
 
     @GetMapping(path = "/workout/{workoutId}")
@@ -27,30 +23,5 @@ public class WorkoutController {
     @GetMapping(path = "/workouts")
     public List<Workout> getAllWorkouts() {
         return workoutService.getAllWorkouts();
-    }
-
-    @GetMapping(path = "/workout/run")
-    public void run() {
-
-        WorkoutDetails workoutDetails = new WorkoutDetails();
-
-        workoutDetails.setDescription("Lay flat on a bench and push the barbell straight away from your chest.");
-        workoutDetails.setReps(12);
-        workoutDetails.setSets(3);
-        workoutDetails.setEquipmentRequired("Bench, barbell,plates");
-        workoutDetails.setWorkoutDuration(30);
-        workoutDetails.setTimeBetweenSets(2);
-        workoutDetails.setWorkoutExampleVideoUrl("musclebuilder.com/bench-press");
-
-        Workout workout = new Workout();
-
-        workout.setWorkoutName("Bench Press");
-        workout.setWorkoutLevel("Medium");
-        workout.setPrimaryWorkoutMuscleGroup("Chest");
-        workout.setSecondaryWorkoutMuscleGroups("Arms");
-        workout.setExerciseType("Strength");
-        workout.setWorkoutDetails(workoutDetails);
-
-        workoutRepository.save(workout);
     }
 }
