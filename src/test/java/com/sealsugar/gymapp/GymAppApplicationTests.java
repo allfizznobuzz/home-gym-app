976d4841a04f8ac6437326b5b1081df6cdf88d64
John <john.h95@gmail.com>
2022-05-15 16:12:42 -0400

    Added Rest Assured dependencies.

--- src/test/java/com/sealsugar/gymapp/GymAppApplicationTests.java
+++ src/test/java/com/sealsugar/gymapp/GymAppApplicationTests.java
@@ -2,12 +2,18 @@ package com.sealsugar.gymapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static io.restassured.RestAssured.given;
 
 @SpringBootTest
 class GymAppApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void test() {
 
+		given().queryParam("CUSTOMER_ID","68195")
+				.queryParam("PASSWORD","1234!")
+				.queryParam("Account_No","1")
+				.when().get("http://demo.guru99.com/V4/sinkministatement.php").then().log()
+				.body();
+	}
 }
